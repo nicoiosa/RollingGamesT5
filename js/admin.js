e868d38bb7b5e4126b089331ad53090795680bf9
nicoIosa <nicolasiosa@hotmail.com>
2023-12-12 12:45:06 -0300

    agregadas funcion editar, borrar y ver detalles

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,4 @@
 import Juego from "./classJuego.js";
-
-// Variables
 const formularioAdminJuegos = document.querySelector(
   "#formularioAdministrarJuegos"
 );
@@ -16,9 +14,12 @@ const requisitos = document.querySelector("#requisitos");
 const desarrollador = document.querySelector("#desarrollador");
 const modalJuego = new bootstrap.Modal(document.querySelector("#modalJuego"));
 const btnAgregar = document.querySelector("#btnAgregar");
+const btnCerrar = document.querySelector("#btnCerrar");
 const listaJuegos = JSON.parse(localStorage.getItem("listaJuegosKey")) || [];
+const tablaJuego = document.querySelector("tbody");
+let modoEditar = false;
+let codigoEditando = null;
 
-// Funciones
 const validarTexto = (elemento, minimo, maximo, nombre) => {
   if (elemento.length > maximo) {
     // Aquí voy a hacer algo.
@@ -34,9 +35,7 @@ const validarTexto = (elemento, minimo, maximo, nombre) => {
   }
 };
 const validarPrecio = (precio) => {
-  // Usar una expresión regular para validar el formato
   const formatoValido = /^\d+(\.\d{1,2})?\s?UD\$$/.test(precio);
-
   if (precio === "Gratis" || precio === "gratis") {
     return true;
   } else if (!formatoValido) {
@@ -46,28 +45,21 @@ const validarPrecio = (precio) => {
       O bien, si no cuesta nada entonces "Gratis".`);
     return false;
   }
-
-  // Extraer el número del precio
   const numeroPrecio = parseFloat(precio);
-
   if (isNaN(numeroPrecio) || numeroPrecio == 0) {
     alert("El valor numérico del precio no es válido.");
     return false;
   }
-
   return true;
 };
 const validarImagen = (imagen) => {
-  // Expresión regular para validar una URL de imagen con extensiones jpeg, jpg, gif, png, bmp o webp
   const formatoImagenValido = /\.(jpeg|jpg|gif|png|bmp|webp)$/i.test(imagen);
-
   if (!formatoImagenValido) {
     alert(
       "La URL no es válida. Debe ser una URL de imagen con una extensión válida (jpeg, jpg, gif, png, bmp, webp)."
     );
     return false;
   }
-
   return true;
 };
 const validarCategoria = (categoria) => {
@@ -92,8 +84,11 @@ const guardarEnLocalStorage = () => {
 const limpiarFormulario = () => {
   formularioAdminJuegos.reset();
 };
+const refrescarTabla = () => {
+  tablaJuego.innerHTML = "";
+  cargaInicial();
+};
 const crearFila = (juego, fila) => {
-  const tablaJuego = document.querySelector("tbody");
   tablaJuego.innerHTML += `
     <tr class="align-middle border-bottom border-secondary border-2">
         <th scope="row"><span class="colorTexto">${fila}</span></th>
@@ -107,17 +102,15 @@ const crearFila = (juego, fila) => {
             <span class="colorTexto">${juego.precio}</span>
         </td>
         <td class="text-end pe-5 pe-md-3">
-            <button class="btn btn-dark btn-sm botonHover border mb-1 mb-md-0"><i class="bi bi-info-square-fill"></i><p class="textoHover">Ver Detalle</p></button>                  
-            <button class="btn btn-dark btn-sm botonHover border mb-1 mb-md-0"><i class="bi bi-pencil-square"><p class="textoHover">Editar</p></i></button>
-            <button class="btn btn-dark btn-sm botonHover border"><i class="bi bi-trash-fill"></i><p class="textoHover">Eliminar</p></button>
+            <button class="btn btn-dark btn-sm botonHover border mb-1 mb-md-0" onclick="verDetalles('${juego.codigo}')"><i class="bi bi-info-square-fill"></i><p class="textoHover">Ver Detalle</p></button>                  
+            <button class="btn btn-dark btn-sm botonHover border mb-1 mb-md-0" onclick="editarJuego('${juego.codigo}')"><i class="bi bi-pencil-square"><p class="textoHover">Editar</p></i></button>
+            <button class="btn btn-dark btn-sm botonHover border" onclick="borrarJuego('${juego.codigo}')"><i class="bi bi-trash-fill"></i><p class="textoHover">Eliminar</p></button>
         </td>
     </tr>
     `;
 };
 const crearJuego = (e) => {
   e.preventDefault();
-  // 1: Tomar info del form. "OK"
-  // 2: Validar entrada. (crear funcion validar)
   const validarTitulo = validarTexto(titulo.value, 2, 30, "titulo");
   const validarPre = validarPrecio(precio.value);
   const validarCat = validarCategoria(categoria.value);
@@ -128,7 +121,6 @@ const crearJuego = (e) => {
   const validarDesc = validarTexto(descripcion.value, 5, 200, "descripcion");
   const validarReq = validarTexto(requisitos.value, 5, 200, "requisitos");
   const validarDev = validarTexto(desarrollador.value, 2, 30, "desarrollador");
-  // 3: Crear objeto pelicula.
   if (
     validarTitulo &&
     validarPre &&
@@ -141,49 +133,119 @@ const crearJuego = (e) => {
     validarReq &&
     validarDev
   ) {
-    const nuevoJuego = new Juego(
-      undefined,
-      titulo.value,
-      precio.value,
-      categoria.value,
-      imagenU.value,
-      imagenD.value,
-      imagenT.value,
-      imagenC.value,
-      descripcion.value,
-      requisitos.value,
-      desarrollador.value
-    );
-    // 4: Guardar lista de juegos en localstorage.
-    listaJuegos.push(nuevoJuego);
+    if (modoEditar) {
+      const juegoEditado = new Juego(
+        codigoEditando,
+        titulo.value,
+        precio.value,
+        categoria.value,
+        imagenU.value,
+        imagenD.value,
+        imagenT.value,
+        imagenC.value,
+        descripcion.value,
+        requisitos.value,
+        desarrollador.value
+      );
+      const posicionJuego = listaJuegos.findIndex(
+        (juego) => juego.codigo === codigoEditando
+      );
+      listaJuegos[posicionJuego] = juegoEditado;
+      cerrarModalJuego()
+    } else {
+      const nuevoJuego = new Juego(
+        undefined,
+        titulo.value,
+        precio.value,
+        categoria.value,
+        imagenU.value,
+        imagenD.value,
+        imagenT.value,
+        imagenC.value,
+        descripcion.value,
+        requisitos.value,
+        desarrollador.value
+      );
+      listaJuegos.push(nuevoJuego);
+    }
     guardarEnLocalStorage();
     limpiarFormulario();
-    // 5: Dibujar el juego nuevo en el html.
-    crearFila(nuevoJuego, listaJuegos.length);
-
+    modoEditar = false;
+    codigoEditando = null;
+    refrescarTabla();
     Swal.fire({
-        icon: "success",
-        title: "VideoJuego Agregado con Éxito",
-        showConfirmButton: false,
-        background: "#343a40",
-        color: "#fff",
-        timer: 3000
+      icon: "success",
+      title: "VideoJuego Agregado con Éxito",
+      showConfirmButton: false,
+      background: "#343a40",
+      color: "#fff",
+      timer: 3000,
     });
   }
 };
 const abrirModalJuego = () => {
   modalJuego.show();
 };
+const cerrarModalJuego = () => {
+  modalJuego.hide();
+  limpiarFormulario();
+};
 const cargaInicial = () => {
   if (listaJuegos.length > 0) {
-    // Dibujar filas.
     listaJuegos.map((itemJuego, posicion) =>
       crearFila(itemJuego, posicion + 1)
-    );    
-  };
-}
-// Más lógica
+    );
+  }
+};
+window.borrarJuego = (codigo) => {
+  Swal.fire({
+    title: "¿Estas seguro?",
+    text: "¡No podras volver atras!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si, borrarlo!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "¡Borrado!",
+        text: "Tu archivo fue borrado.",
+        icon: "success",
+      });
+      const posicionJuego = listaJuegos.findIndex(
+        (juego) => juego.codigo === codigo
+      );
+      listaJuegos.splice(posicionJuego, 1);
+      guardarEnLocalStorage();
+      tablaJuego.removeChild(tablaJuego.children[posicionJuego]);
+    }
+  });
+};
+window.editarJuego = (codigo) => {
+  const juegoAEditar = listaJuegos.find((juego) => juego.codigo === codigo);
+  if (juegoAEditar) {
+    modoEditar = true;
+    codigoEditando = codigo;
+    document.querySelector("#titulo").value = juegoAEditar.titulo;
+    document.querySelector("#precio").value = juegoAEditar.precio;
+    document.querySelector("#categoria").value = juegoAEditar.categoria;
+    document.querySelector("#imagenU").value = juegoAEditar.imagenU;
+    document.querySelector("#imagenD").value = juegoAEditar.imagenD;
+    document.querySelector("#imagenT").value = juegoAEditar.imagenT;
+    document.querySelector("#imagenC").value = juegoAEditar.imagenC;
+    document.querySelector("#descripcion").value = juegoAEditar.descripcion;
+    document.querySelector("#requisitos").value = juegoAEditar.requisitos;
+    document.querySelector("#desarrollador").value = juegoAEditar.desarrollador;
+    abrirModalJuego();
+  }
+};
+window.verDetalles = (codigo) => {
+  window.location.href =
+    window.location.origin + "/pages/detail.html?codigo=" + codigo;
+};
+
 formularioAdminJuegos.addEventListener("submit", crearJuego);
 btnAgregar.addEventListener("click", abrirModalJuego);
-
+btnCerrar.addEventListener("click", cerrarModalJuego);
 cargaInicial();
